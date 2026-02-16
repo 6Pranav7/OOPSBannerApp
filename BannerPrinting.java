6b1ea05244d6960607c0dc12c0d500fe39399548
6Pranav7 <pm2848@srmist.edu.in>
2026-02-16 23:13:31 +0530

    UC2 Banner print statements

---
+++ BannerPrinting.java
@@ -0,0 +1,14 @@
+package CoreProgrammingApps.OOPSBannerApp;
+
+public class BannerPrinting{
+    public static void main(String[] args) {
+        System.out.println("  *** " + "   ***" + "   ***** "  +   "  ***** ");
+        System.out.println(" **  ** " +"**   **" + " **  **"  +   "  ** ");
+        System.out.println("**    **" +"**    **" + "**   **" +  " ** ");
+        System.out.println("**     **" +"**   **" + "**  **"  +    " *****" );
+        System.out.println("**     **" +"**   **" + "**"      +    "      **" );
+        System.out.println(" **   ** " +"**  ** " + "** "     +   " ** **");
+        System.out.println("   *** " +    "***" +  " ** "     + "  ***** ");
+    
+}
+}
