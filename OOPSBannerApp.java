1185a63e770bbc3b26693678dc0f10fbf4a6c033
6Pranav7 <pm2848@srmist.edu.in>
2026-02-17 09:45:05 +0530

    Coded to Render OOPS as Banner using Print Statments

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,8 +1,19 @@
 package CoreProgrammingApps.OOPSBannerApp;
 
-public class OOPSBannerApp {
+public class OOPSBannerApp{
     public static void main(String[] args) {
         System.out.println("OOPS");
-    }
+        
+        
+            System.out.println("  *** " + "   ***" + "   ***** "  +   "  ***** ");
+            System.out.println(" **  ** " +"**   **" + " **  **"  +   "  ** ");
+            System.out.println("**    **" +"**    **" + "**   **" +  " ** ");
+            System.out.println("**     **" +"**   **" + "**  **"  +    " *****" );
+            System.out.println("**     **" +"**   **" + "**"      +    "      **" );
+            System.out.println(" **   ** " +"**  ** " + "** "     +   " ** **");
+            System.out.println("   *** " +    "***" +  " ** "     + "  ***** ");
     
+    }
+
 }
+
