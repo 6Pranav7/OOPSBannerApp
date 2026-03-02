708bbc72a29e9f4717d1f0346a202ef8a24fbf8d
6Pranav7 <pm2848@srmist.edu.in>
2026-03-02 09:40:34 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,10 +1,11 @@
 package CoreProgrammingApps.OOPSBannerApp;
 
+//UC1
 public class OOPSBannerApp{
     public static void main(String[] args) {
         System.out.println("OOPS");
         
-        
+            //UC2
             System.out.println("  *** " + "   ***" + "   ***** "  +   "  ***** ");
             System.out.println(" **  ** " +"**   **" + " **  **"  +   "  ** ");
             System.out.println("**    **" +"**    **" + "**   **" +  " ** ");
@@ -13,7 +14,7 @@ public class OOPSBannerApp{
             System.out.println(" **   ** " +"**  ** " + "** "     +   " ** **");
             System.out.println("   *** " +    "***" +  " ** "     + "  ***** ");
 
-
+                //UC3
                 System.out.println(String.join(" ", " ***** ", " ***** ", " ***** ", " ***** "));
                 System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*      "));
                 System.out.println(String.join(" ", "*     *", "*     *", "*     *", "*      "));
