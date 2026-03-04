304366a49f09c577035789552fe80fb8710bb84d
6Pranav7 <pm2848@srmist.edu.in>
2026-03-04 19:36:14 +0530

    OOPSBannerApp6.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,19 +1,51 @@
-package CoreProgrammingApps.OOPSBannerApp;
-
-public class OOPSBannerApp{
-    public static void main(String[] args) {
-        System.out.println("OOPS");
+public class OOPSBannerApp6 {
+                public static void main(String[] args) {
+                        String[] O = getO();
+                        String[] P = getP();
+                        String[] S = getS();
         
+                        // Render banner row by row
+                        for (int i = 0; i < O.length; i++) {
+                                System.out.println(
+                                                String.join("    ",
+                                                                O[i], O[i], P[i], S[i])
+                                );
+                        }
+                }
         
-            System.out.println("  *** " + "   ***" + "   ***** "  +   "  ***** ");
-            System.out.println(" **  ** " +"**   **" + " **  **"  +   "  ** ");
-            System.out.println("**    **" +"**    **" + "**   **" +  " ** ");
-            System.out.println("**     **" +"**   **" + "**  **"  +    " *****" );
-            System.out.println("**     **" +"**   **" + "**"      +    "      **" );
-            System.out.println(" **   ** " +"**  ** " + "** "     +   " ** **");
-            System.out.println("   *** " +    "***" +  " ** "     + "  ***** ");
-    
-    }
-
-}
-
+                public static String[] getO() {
+                        return new String[]{
+                                        " ***** ",
+                                        "* *",
+                                        "* *",
+                                        "* *",
+                                        "* *",
+                                        "* *",
+                                        " ***** "
+                        };
+                }
+        
+                public static String[] getP() {
+                        return new String[]{
+                                        " ***** ",
+                                        "* *",
+                                        " ***** ",
+                                        "* ",
+                                        "* ",
+                                        "* ",
+                                        "* "
+                        };
+                }
+        
+                public static String[] getS() {
+                        return new String[]{
+                                        " ***** ",
+                                        "* ",
+                                        " ***** ",
+                                        "      *",
+                                        "      *",
+                                        "* *",
+                                        " ***** "
+                        };
+                }
+        }
